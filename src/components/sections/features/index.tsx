90725e2ad5bed2fdd08125cf9619b490d7af180f
Tamer <tamertawfiklxr@gmail.com>
2025-03-09 07:39:29 +0200

    chore: add features section

---
+++ src/components/sections/features/index.tsx
@@ -0,0 +1,10 @@
+import { TextGenerateEffect } from "./text-generate-effect";
+
+export function WhySection() {
+  return (
+    <TextGenerateEffect
+      className="md:pt-28 pb-12 md:pb-32 text-2xl	md:text-3xl max-w-[1220px] container md:leading-[55px] font-medium mb-12"
+      words="Team up with like-minded developers and build something amazing.  Collaborate on real-world projects, sharpen your skills, and expand your portfolio. Join hackathons, contribute to open source, and kickstart your SaaS journey."
+    />
+  );
+}
