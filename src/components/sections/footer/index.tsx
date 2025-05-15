81642edc14be4b64dced9b8a5142d510929cf448
Tamer <tamertawfiklxr@gmail.com>
2025-05-15 21:55:04 +0300

    chore: enhance footer section

--- src/components/sections/footer/index.tsx
+++ src/components/sections/footer/index.tsx
@@ -1,13 +1,29 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
+import { Suspense } from "react";
+import SectionContainer from "@/components/sections/section-container";
+
 export function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full py-6 bg-white dark:bg-black border-t">
-      <div className="container mx-auto px-4">
-        <p className="text-center text-sm text-foreground dark:text-muted-foreground">
-          &copy; {currentYear} Teamup Circle. All rights reserved.
-        </p>
-      </div>
+    <footer className="w-full !py-0">
+      <SectionContainer>
+        <div className="border-default flex justify-between border-t pt-8">
+          <small className="text-sm text-muted-foreground">
+            &copy; {currentYear} made with ❤️ by{" "}
+            <Link target="_blank" href="https://github.com/TamerTawfik">
+              Tamer Tawfik
+            </Link>
+          </small>
+          <div>
+            <Suspense fallback={<Skeleton className="h-8 w-8 rounded-full" />}>
+              <ModeToggle className="border-none -mt-4" />
+            </Suspense>
+          </div>
+        </div>
+      </SectionContainer>
     </footer>
   );
 }
